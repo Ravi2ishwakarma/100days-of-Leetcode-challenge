e01e63ddf75a2c245cea4037686db28522af5b18
Ravi Vishwakarma <ravivishwakarma20065@gmail.com>
2025-03-20 13:35:55 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,1 +1,21 @@
+// If any number is palindrome then return true else false
 
+class Solution()
+  public boolean Ispalindrome(int a){
+  if(a<0){
+   return false;
+  }
+  int n= a;
+  int revnum=0;
+  while(n>0){
+    int d= n%10;
+    revnum= revnum*10 +d;
+    n= n/10;
+      }
+   if(a==revnum){
+     return true;
+    }
+   else{
+     return false;
+    }
+  }
